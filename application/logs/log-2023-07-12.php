d75928440868c4104f8e45bd59b0c7cbaa36ef32
soha mahmoud hussien <finishizer@gmail.com>
2023-07-13 19:10:18 +0300

    first commit

---
+++ application/logs/log-2023-07-12.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-07-12 16:36:59 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 16:38:01 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 16:44:02 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:44:04 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:44:05 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:44:06 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:44:46 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:44:48 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:44:49 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:45:11 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:45:14 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:45:15 --> Severity: error --> Exception: Table 'codeigniter.tblsessions' doesn't exist /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2023-07-12 16:45:29 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 18:55:14 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 18:55:15 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 18:55:16 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 18:55:17 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 18:55:18 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-07-12 18:55:19 --> Severity: error --> Exception: Access denied for user 'root'@'localhost' /home/sowayha/Documents/Orgwany/finishizer_erp2023/system/database/drivers/mysqli/mysqli_driver.php 203
